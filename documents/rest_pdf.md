40e45749ffde5c603960ef6129f13dd7c31e5164
woong <woong@lazypic.org>
2023-03-27 17:58:23 +0900

    add token

--- documents/rest_pdf.md
+++ documents/rest_pdf.md
@@ -6,6 +6,6 @@ PDF Restapi 입니다.
 
 | URI | Description | Attributes | Curl Example |
 | --- | --- | --- | --- |
-| /api/pdf-to-json | pdf를 전송하고 json을 반환합니다. | pdf | curl -X POST -F 'pdf=@/path/to/pdf/file.pdf' http://localhost/api/pdf-to-json
+| /api/pdf-to-json | pdf를 전송하고 json을 반환합니다. | pdf | curl -X POST -F 'pdf=@/path/to/pdf/file.pdf' -H "Authorization: Basic {TOKEN}" http://localhost/api/pdf-to-json
 "
 
--- handler_pdf.go
+++ handler_pdf.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/unidoc/unipdf/v3/extractor"
 	"github.com/unidoc/unipdf/v3/model"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func handlerAPIPdfToJson(w http.ResponseWriter, r *http.Request) {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(*flagMongoDBURI))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// token 체크
+	_, _, err = TokenHandlerV2(r, client)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	// 파일 읽기
 	file, _, err := r.FormFile("pdf")
 	if err != nil {
