a710c53018ade8f502607e6e04a4310c39a28779
hanwoong kim <hello@lazypic.org>
2023-04-04 12:52:03 +0900

    go vet 에러서에서 나오는 문장들을 수정함.

--- db_status.go
+++ db_status.go
@@ -15,7 +15,7 @@ func AllStatusV2(client *mongo.Client) ([]Status, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	findOptions := options.Find()
-	findOptions.SetSort(bson.D{{"order", -1}})
+	findOptions.SetSort(bson.D{{Key: "order", Value: -1}})
 	var status []Status
 	cursor, err := collection.Find(ctx, bson.D{}, findOptions)
 	if err != nil {
