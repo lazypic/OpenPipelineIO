6765eeb707632d003c5a8bce7ce690d6fa65338a
hanwoong kim <hello@lazypic.org>
2023-04-04 17:01:57 +0900

    upload 완료.

--- assets/js/upload-scenario-pdf.js
+++ assets/js/upload-scenario-pdf.js
@@ -1,38 +1,50 @@
 
 
 
-// Dropzone.js 설정
-Dropzone.options.pdfUpload = {
-    url: '/api/pdf-to-json',
-    paramName: 'file', // 서버에 전송될 파일의 매개변수 이름
-    maxFilesize: 10, // MB
-    acceptedFiles: 'application/pdf',
-    init: function () {
-        this.on('sending', function (file, xhr) {
-            console.log(document.getElementById("project").options[document.getElementById("project").selectedIndex].value)
-            console.log(document.getElementById("version").value)
-            console.log(document.getElementById("part").value)
-            const formData = new FormData();
-            // 프로젝트와 버전 정보를 전송 데이터에 추가
-            formData.append('project', document.getElementById("project").options[document.getElementById("project").selectedIndex].value);
-            formData.append('version', document.getElementById("version").value);
-            formData.append('part', parseInt(document.getElementById("part").value));
+// 수동으로 업로드 하는 방법을 찾아야 겠다.
+const dropZone = document.getElementById('drop-zone');
 
-             // Authorization 헤더를 요청에 추가
-             xhr.setRequestHeader('Authorization', "Basic "+ document.getElementById("token").value);
+// 파일 드롭 처리 함수
+async function handleFileDrop(event) {
+    event.preventDefault();
 
-             // 파일과 함께 폼 데이터 전송
-             xhr.send(formData);
+    const file = event.dataTransfer.files[0];
+    if (!file) return;
+
+    // FormData 생성 및 파일과 프로젝트 정보 추가
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('project', document.getElementById("project").options[document.getElementById("project").selectedIndex].value);
+    formData.append('version', document.getElementById("version").value);
+    formData.append('part', parseInt(document.getElementById("part").value));
+
+    try {
+        const response = await fetch('/api/pdf-to-json', {
+            method: 'POST',
+            headers: {
+                'Authorization': "Basic "+ document.getElementById("token").value,
+            },
+            body: formData,
         });
 
-        // 성공적으로 업로드된 경우, 응답 JSON 출력
-        this.on('success', function (file, response) {
-            console.log('업로드 성공: ', response);
-        });
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log('전송 성공:', jsonResponse);
+        } else {
+            console.error('전송 실패:', response.statusText);
+        }
+    } catch (error) {
+        console.error('전송 중 오류 발생:', error);
+    }
+}
 
-        // 업로드 실패한 경우, 에러 메시지 출력
-        this.on('error', function (file, errorMessage) {
-            console.error('업로드 실패: ', errorMessage);
-        });
-    },
-};
+// 드래그 앤 드롭 이벤트 처리 함수
+function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+}
+
+// 이벤트 리스너 등록
+dropZone.addEventListener('drop', handleFileDrop);
+dropZone.addEventListener('dragover', handleDragOver);
+
