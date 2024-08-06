caf64bdd6f362c7bb72cf047f8e03fbf56b568ce
hanwoong kim <khw7096@gmail.com>
2024-08-06 12:53:27 +0900

    update comment

--- main.go
+++ main.go
@@ -38,9 +38,9 @@ var (
 	// TEMPLATES 값은 웹서버 실행전 사용할 템플릿이다.
 	TEMPLATES = template.New("")
 	// SHA1VER  은 Git SHA1 값이다.
-	SHA1VER = "26b300a004abae553650c924514dc550e7385c9e" // 첫번째 커밋
+	SHA1VER = "26b300a004abae553650c924514dc550e7385c9e" // First commit SHA1
 	// BUILDTIME 은 빌드타임 시간이다.
-	BUILDTIME = "2012-11-08T10:00:00" // 최초로 만든 시간
+	BUILDTIME = "2012-11-08T10:00:00" // First commit time
 	// CachedAdminSetting 은 서비스 시작전 어드민 셋팅값을 메모리에 넣어서 사용되는 변수이다.
 	CachedAdminSetting = Setting{}
 
@@ -48,10 +48,10 @@ var (
 	flagDBIP           = flag.String("dbip", DBIP+DBPORT, "mongodb ip and port")                                                            // mgo용 mongoDB 주소
 	flagMongoDBURI     = flag.String("mongodburi", fmt.Sprintf("mongodb://%s%s", DBIP, DBPORT), "mongoDB URI ex)mongodb://localhost:27017") //mongo-driver용 인수
 	flagDBName         = flag.String("dbname", DBNAME, "mongodb db name")                                                                   // mongoDB DB이름
-	flagDebug          = flag.Bool("debug", false, "디버그모드 활성화")
+	flagDebug          = flag.Bool("debug", false, "debug mode")
 	flagHTTPPort       = flag.String("http", "", "Web Service Port number.")          // 웹서버 포트
 	flagVersion        = flag.Bool("version", false, "Print Version")                 // 버전
-	flagCookieAge      = flag.Int64("cookieage", 168, "cookie age (hour)")            // 기본 일주일(168시간)로 설정한다. 참고: MPAA 기준 4시간이다.
+	flagCookieAge      = flag.Int64("cookieage", 168, "cookie age (hour)")            // 기본 일주일(168시간)로 설정한다. 참고: MPAA 기준 Cookie save time is 4H.
 	flagThumbnailAge   = flag.Int("thumbnailage", 1, "thumbnail image age (seconds)") // 썸네일 업데이트 시간. 3600초 == 1시간
 	flagAuthmode       = flag.Bool("authmode", false, "restAPI authorization active") // restAPI 이용시 authorization 활성화
 	flagCertFullchanin = flag.String("certfullchanin", fmt.Sprintf("/etc/letsencrypt/live/%s/fullchain.pem", DNS), "certification fullchain path")
@@ -60,11 +60,11 @@ var (
 	flagProcessBufferSize = flag.Int("processbuffersize", 100, "process buffer size") // 최대 대기 리스트
 	flagMaxProcessNum     = flag.Int("maxprocessnum", 4, "max process number")        // 최대 연산 갯수
 	flagProcessDuration   = flag.Int64("processduration", 10, "process duration")
-	flagReviewRender      = flag.Bool("reviewrender", false, "리뷰 렌더링을 허용하는 옵션")
-	flagScanPlateRender   = flag.Bool("scanplaterender", false, "ScanPlate 렌더링을 허용하는 옵션")
+	flagReviewRender      = flag.Bool("reviewrender", false, "review render mode")
+	flagScanPlateRender   = flag.Bool("scanplaterender", false, "scanplate render mode")
 
 	// Commandline Args
-	flagHelp              = flag.Bool("help", false, "자세한 도움말을 봅니다.")
+	flagHelp              = flag.Bool("help", false, "help")
 	flagID                = flag.String("id", "", "user id")
 	flagAccessLevel       = flag.Int("accesslevel", -1, "edit user Access Level")
 	flagSignUpAccessLevel = flag.Int("signupaccesslevel", 0, "signup access level")
@@ -148,9 +148,9 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// 어드민설정을 한번 저장한다. CachedAdminSetting값은 매번 DB를 호출하면 안되는 작업에서 사용된다.
+		// 어드민설정을 저장한다. CachedAdminSetting값은 매번 DB 호출하지 않는 처리에 사용된다.
 		CachedAdminSetting = admin
-		// 만약 Admin설정에 ThumbnailRootPath가 잡혀있다면 그 값을 이용한다.
+		// 만약 Admin 설정에 ThumbnailRootPath가 잡혀있다면 그 값을 이용한다.
 		if admin.ThumbnailRootPath == "" {
 			log.Println("admin 설정창의 thumbnail 경로지정이 필요합니다.")
 		}
