951d57bbb12546c6953727e0384716792fc8e8ed
jason <jason@lazypic.org>
2024-11-25 07:21:33 +0000

    change ko log to en log.

--- timefunc.go
+++ timefunc.go
@@ -9,7 +9,7 @@ import (
 func str2time(str string) time.Time {
 	t, err := time.Parse(time.RFC3339, str)
 	if err != nil {
-		log.Println("시간을 바꿀 수 없습니다.")
+		log.Println("Time cannot be converted to a string.")
 		//리턴은 1970년 초기값을 반환한다.
 	}
 	return t
