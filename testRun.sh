8e097448f22ac9325f6d5b7bd616f13b497dfb4c
jason <jason@lazypic.org>
2025-01-08 14:43:26 +0000

    rm not use menu and edit test build script for freebsd

--- testRun.sh
+++ testRun.sh
@@ -1,3 +1,5 @@
 go run assets/asset_generate.go
-go install -ldflags "-X main.SHA1VER=`git rev-parse HEAD` -X main.BUILDTIME=`date -u +%Y-%m-%dT%H:%M:%S`"
-$HOME/go/bin/openpipelineio -http :80 -reviewrender -scanplaterender --debug
+go install
+sudo service OpenPipelineIO restart
+#go install -ldflags "-X main.SHA1VER=`git rev-parse HEAD` -X main.BUILDTIME=`date -u +%Y-%m-%dT%H:%M:%S`"
+#$HOME/go/bin/openpipelineio -http :80 -reviewrender -scanplaterender --debug
